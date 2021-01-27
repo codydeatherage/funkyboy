3bf7aa76d2eedea523b1d743e48be778749693f0
codydeatherage <43896794+codydeatherage@users.noreply.github.com>
2021-01-26 23:24:11 -0800

    Add files via upload

---
+++ commands/clear.js
@@ -0,0 +1,7 @@
+const ytdl = require('ytdl-core');
+
+module.exports = {
+    name: 'clear',
+    description: 'Clear selection',
+    
+}
---
+++ index.js
@@ -0,0 +1,48 @@
+const Discord = require('discord.js');
+const ffmpeg = require('ffmpeg');
+const fs = require('fs');
+const { prefix, token } = require('./config.json');
+
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const cmdFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+
+for(const file of cmdFiles) {
+    const cmd = require(`./commands/${file}`);
+    client.commands.set(cmd.name, cmd);
+}
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.on('message', async message => {
+    if (message.author.bot || !message.content.startsWith(prefix)) return;
+    
+    // const args = message.content.slice(prefix.length).trim().split(/ +/);
+    // const cmd = args.shift().toLowerCase();
+
+    // if(cmd === 'clear'){
+    //     client.commands.get('clear').execute(message, args);
+    // } else if(cmd === 'play'){
+    //     client.commands.get('play').execute(message, args);
+    // } else if(cmd === 'stop'){
+    //     client.commands.get('stop').execute(message, args);
+    // }
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const cmdName = args.shift().toLowerCase();
+    const cmd = client.commands.get(cmdName);
+
+    try{
+        cmd.execute(message,args)
+    } catch(err){
+        console.error(error);
+        message.reply('There was an error trying to execute that command!');
+    }
+
+
+
+})
+
+client.login(token);
